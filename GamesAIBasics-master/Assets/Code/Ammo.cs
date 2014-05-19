805cd2e41f7b02620a961912a114e69db3a1c18d
Paul Conway <paulconway0@gmail.com>
2014-05-19 18:34:32 +0100

    Spawners Added

---
+++ GamesAIBasics-master/Assets/Code/Ammo.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ammo : MonoBehaviour {
+
+	float rotSpeed;
+
+	void Start ()
+	{
+		rotSpeed = Random.Range (20f, 40f);
+	}
+
+	void Update () 
+	{
+		transform.Rotate(transform.up * rotSpeed * Time.deltaTime);
+	}
+}
---
+++ GamesAIBasics-master/Assets/Code/GameManager.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameManager : MonoBehaviour {
+
+	Object bot, ammo;
+	int botCount = 5, ammoCount = 10;
+	List<GameObject> bots = new List<GameObject>();
+	List<GameObject> ammos = new List<GameObject>();
+	Vector3 newPos;
+
+	void Start () 
+	{
+       	//leader.GetComponent<StateMachine>().SwitchState(new IdleState(leader, teaser));
+		bot = Resources.Load ("Bot");
+		ammo = Resources.Load ("Ammo");
+		SpawnBots();
+		SpawnAmmo(5);
+	}
+
+	void SpawnBots()
+	{
+		for(int i = 0; i < botCount; i++)
+		{
+			newPos = new Vector3(Random.Range (-20f, 20f), 0f, Random.Range(-20f, 20f));
+			GameObject botIns = Instantiate(bot, newPos, Quaternion.identity) as GameObject;
+			botIns.name = "Bot";
+			botIns.tag = "BotTag";
+			bots.Add(botIns);
+		}
+	}
+
+	void SpawnAmmo(int quantity)
+	{
+		for(int i = 0; i < ammoCount; i++)
+		{
+			newPos = new Vector3(Random.Range (-20f, 20f), 0f, Random.Range(-20f, 20f));
+			GameObject ammoIns = Instantiate(ammo, newPos, Quaternion.identity) as GameObject;
+			ammoIns.name = "Ammo";
+			ammoIns.tag = "AmmoTag";
+			//add ammo quantity
+			bots.Add(ammoIns);
+		}
+	}
+}
